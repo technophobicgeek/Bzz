3c59dc484d92038db41482908cad46a7244ce826
Raj Bandyopadhyay <technophobicgeek@battlestar.(none)>
2010-11-07 01:11:17 -0400

    modified screens

--- app/Partner/partner.rb
+++ app/Partner/partner.rb
@@ -1,5 +1,11 @@
 # The model has already been created by the framework, and extends Rhom::RhomObject
 # You can add more methods here
+# Do I want a PropertyBag or FixedSchema model?
+#	- Some values need to be optional, some unique, some mandatory
+#	- Can check that in controller or DB
+#	- Maybe FixedSchema is better, since allows DB checking
+
+
 class Partner
   include Rhom::PropertyBag
 
