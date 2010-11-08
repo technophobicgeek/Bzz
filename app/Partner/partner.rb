120b72a0b90729f9869f869cb02d9d1cfd915555
Raj Bandyopadhyay <technophobicgeek@battlestar.(none)>
2010-11-07 23:38:21 -0500

    FixedSchema db model for contacts, stores data

--- app/Partner/partner.rb
+++ app/Partner/partner.rb
@@ -7,10 +7,26 @@
 
 
 class Partner
-  include Rhom::PropertyBag
+  include Rhom::FixedSchema
 
   # Uncomment the following line to enable sync with Partner.
   # enable :sync
+  
+  # model-specific code
+  
+  set schema_version, '0.1' # pour data migration
+  
+  #object properties
+  property :bzzid, :string
+  property :name, :string
+  property :nick, :string
+  property :phone, :string
+  property :email, :string
+  property :twitter, :string
 
-  #add model specifc code here
+  #indexes
+  unique_index :by_bzzid, [:bzzid]
+  unique_index :by_phone, [:phone]
+  unique_index :by_email, [:email]
+  unique_index :by_twitter, [:twitter]
 end
